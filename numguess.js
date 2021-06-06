ede80996f9dff87c948e8f8b7de5e8c0df0db10a
Tasfiq97 <71073311+Tasfiq97@users.noreply.github.com>
2021-06-06 12:46:17 +0600

    new commit

---
+++ numguess.js
@@ -0,0 +1,51 @@
+let inputNum = document.getElementById("input-num");
+
+let startGame = document.getElementById("start-btn");
+
+let resetGame = document.getElementById("reset-btn");
+
+let numCorr = document.getElementById("input-corr");
+
+let numShow = document.getElementById("input-show");
+
+let numGuess = Math.floor(Math.random() * 100);
+
+startGame.addEventListener("click", function () {
+  // console.log("click")
+  let getVal = inputNum.value;
+  // console.log(getVal)
+  let alertNum = document.createElement("p");
+  numCorr.appendChild(alertNum);
+
+  if (getVal == numGuess) {
+    alertNum.innerHTML = "awesome!!! you got the number 🌟";
+  } else if (getVal > numGuess) {
+    alertNum.innerHTML = "You are too high..keep trying 💪";
+  } else {
+    alertNum.innerHTML = "You are too low...keep trying 💪";
+  }
+  let numberHis = document.createElement("h3");
+
+  numShow.appendChild(numberHis);
+
+  numberHis.innerHTML = "you guessed" + " " + getVal;
+
+  setTimeout(() => {
+    numCorr.removeChild(alertNum);
+  }, 2000);
+
+  resetGame.addEventListener("click", function () {
+    // console.log("click")
+    inputNum.value = "";
+    numberHis.innerHTML = "";
+  });
+});
+
+//the random number function//
+
+randomNum = () => {
+  //   console.log(numGuess)
+};
+randomNum();
+
+/// function for reset all//
